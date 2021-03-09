6077e367417224c3ec0ec066181650c036dfb965
Luisa <lhurtado372@gmail.com>
2021-03-09 17:01:45 -0500

    detecta evento hover

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // This is where the magic happens
+$(document).ready(function() {
+    //detectar mouse sobre el elemento
+    $("a").hover(function() {
+      $(this).css("color","#EA178C");
+      },
+     function() {
+      $(this).css("color","");
+      });
+
+})
