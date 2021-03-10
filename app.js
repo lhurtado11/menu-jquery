57a21b12f601dc7ab79f87996a72deadd5f3f0e0
Luisa <lhurtado372@gmail.com>
2021-03-10 15:25:48 -0500

    limpia clase active en los elementos

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // This is where the magic happens
 $(document).ready(function() {
-    //detectar mouse sobre el elemento
+   
     $("a").hover(function() {
       $(this).css("color","#EA178C");
       },
@@ -8,7 +8,11 @@ $(document).ready(function() {
       $(this).css("color","");
       }
     );
-    //detecta click sobre el elemento, adiciona clase active al elemento
+
+    $('a').on('click', function() {
+      $('a').removeClass('active');
+    });
+
     $('a').on('click', function() {
      $(this).addClass('active')
     });
